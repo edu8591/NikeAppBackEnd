a14e2c9c0c4c4e59629fa3f2a6185eba7f1705d6
Eduardo Serrano <eduardoserrano8591@gmail.com>
2023-04-29 05:08:15 -0500

    creates actions for getting products

---
+++ src/databse/products.js
@@ -0,0 +1,11 @@
+const { ObjectId } = require("mongodb");
+const { products } = require("./db");
+
+const getAllProducts = async () => {
+  return await products.find({}).toArray();
+};
+const getProductById = async (id) => {
+  return await products.findOne({ _id: new ObjectId(id) });
+};
+
+module.exports = { getAllProducts, getProductById };
